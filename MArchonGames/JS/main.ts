8190a90807c0f04afbc8f07ab39f7b6b91047f7d
MArchaon98 <mc760@outlook.com>
2025-03-02 20:09:42 -0700

    fixed src path

--- MArchonGames/JS/main.ts
+++ MArchonGames/JS/main.ts
@@ -1,7 +1,8 @@
 'use strict';
 
 function welcomeUser() {
-  let userName = prompt("Welcome! Please enter your name:");
+  let userName: string | null = prompt("Welcome! Please enter your name:");
+
   if (userName) {
     document.getElementById("welcome-message").innerText = Welcome, ${username}! Welcome to the site;
   }
